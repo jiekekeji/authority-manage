a4007e704f7a791739469cccc8dcc2d62cc185ef
jiekekeji <jieke_rose@foxmail.com>
2017-10-13 16:51:33 +0800

    init

---
+++ src/components/CNavLeft.vue
@@ -0,0 +1,57 @@
+<!--顶部导航组件-->
+<template>
+  <div class="container" style="font-size: 12px">
+    <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" theme="dark">
+      <el-submenu index="1">
+        <template slot="title">导航一</template>
+        <el-menu-item-group title="分组一">
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="1-4-1">选项1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="2">导航二</el-menu-item>
+      <el-menu-item index="3">导航三</el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {}
+    },
+    components: {},
+    activated(){
+
+    },
+    beforeRouteLeave (to, from, next) {
+      next();
+    },
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss" scoped="" type="text/scss">
+
+  .container {
+    height: 48px;
+    width: 200px;
+    background-color: rebeccapurple;
+  }
+
+</style>
--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="container">
-    <CNavTop></CNavTop>
+    <div class="top">
+      <CNavTop></CNavTop>
+    </div>
+    <div class="left">
+      <CNavLeft></CNavLeft>
+    </div>
+    <div class="right"></div>
 
 
   </div>
@@ -8,12 +14,14 @@
 
 <script>
   import CNavTop from "../components/CNavTop.vue"
+  import CNavLeft from "../components/CNavLeft.vue"
   export default {
     data () {
       return {}
     },
     components: {
-      CNavTop
+      CNavTop,
+      CNavLeft
     },
     activated(){
 
@@ -28,5 +36,10 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss" scoped="" type="text/scss">
 
+  .container {
+    display: flex;
+    min-width: 1200px;
+    width: 100%;
+  }
 
 </style>
